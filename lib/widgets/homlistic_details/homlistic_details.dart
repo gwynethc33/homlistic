c15ac335b710ac37da8cc6cf4bb3cfa94c8d91c2
Gwyneth Chan <172159215+gwynethc33@users.noreply.github.com>
2025-06-24 11:13:47 -0700

    added git ignore

--- lib/widgets/homlistic_details/homlistic_details.dart
+++ lib/widgets/homlistic_details/homlistic_details.dart
@@ -33,7 +33,7 @@ class HomlisticDetails extends StatelessWidget {
                     vertical: 60,
                   ),
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 255, 18, 140),
+                    color: const Color.fromARGB(255, 44, 73, 120),
                   ),
                   child: FadeInText(
                     text:
